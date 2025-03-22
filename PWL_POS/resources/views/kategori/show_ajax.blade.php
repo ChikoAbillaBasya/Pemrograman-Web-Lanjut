0818ed12d5af5fe6548d974526137ddc3fd17968
Chikoabillabasya <galangrizky767@gmail.com>
2025-03-22 23:48:26 +0700

    Jobsheet 6

---
+++ PWL_POS/resources/views/kategori/show_ajax.blade.php
@@ -0,0 +1,49 @@
+@if (empty($kategori))
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Kesalahan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-danger">
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan!</h5>
+                    Data kategori tidak ditemukan.
+                </div>
+                <button type="button" class="btn btn-warning" data-dismiss="modal">Kembali</button>
+            </div>
+        </div>
+    </div>
+@else
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Detail Kategori</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-bordered">
+                    <tr>
+                        <th width="25%">ID Kategori</th>
+                        <td>{{ $kategori->kategori_id }}</td>
+                    </tr>
+                    <tr>
+                        <th>Kode Kategori</th>
+                        <td>{{ $kategori->kategori_kode }}</td>
+                    </tr>
+                    <tr>
+                        <th>Nama Kategori</th>
+                        <td>{{ $kategori->kategori_nama }}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+@endif
